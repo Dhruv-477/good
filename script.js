398aa8c25b706d9d4596f281d0f4a4036ddcd896
Dhruv-477 <dhruv04072007@gmail.com>
2024-06-14 15:01:34 +0530

    Sherrr!!!

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("Hey are you crazy !?");
+let number = prompt("Enter a number, Fast !!!");
+let yoyoyo = confirm("do you want to print it ?");
+if(yoyoyo){
+    document.write(`${number}`);
+}else{
+    alert("You are g@y !!");
+}
